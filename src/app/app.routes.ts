1ae8397ab6ea8a50c10e54d83d8f1af03b1419e7
Superdaadi <dadimanuelsch@gmail.com>
2025-06-01 23:15:17 +0200

    1st Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { GoogleComponent } from './google/google.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: GoogleComponent},
+];
