4d502332ccceab0c8bba6bd4c1690284bfdbb8b7
Danny-123-code <d.staller2007@gmail.com>
2025-07-07 11:05:20 +0200

    google

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { GoogleComponent } from './google/google.component';
 import { HompageComponent } from './hompage/hompage.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 export const routes: Routes = [
     {path: 'e', component: GoogleComponent},
     {path: 'home', component:HompageComponent},
-    {path: 'nav', component:NavbarComponent}
+    {path: 'nav', component:NavbarComponent},
+    {path: 'side', component:SidebarComponent}
 ];
