38f60f9915a3b22b09ac4cd7860e2c8ec098c649
Superdaadi <dadimanuelsch@gmail.com>
2025-09-24 17:09:49 +0200

    Added login Screen for mail and pw && blancscreen fix && cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { GoogleComponent } from './google/google.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: '', component: GoogleComponent},
+    {path: 'login', component: LoginComponent},
 ];
--- src/app/google/google.component.ts
+++ src/app/google/google.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-google',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './google.component.css'
 })
 export class GoogleComponent {
+
+  constructor(private router: Router) {}
+
+
+  toLogin() {
+    this.router.navigate(['login'])
+  }
 
 }
